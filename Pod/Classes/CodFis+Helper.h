b28f09779525401ef970e93d5fa1b5c609bdcc53
Giuseppe Nucifora <me@giuseppenucifora.com>
2015-07-08 17:12:35 +0200

    first commit

---
+++ Pod/Classes/CodFis+Helper.h
@@ -0,0 +1,13 @@
+//
+//  CodFis+Helper.h
+//  Pods
+//
+//  Created by Giuseppe Nucifora on 08/07/15.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CodFis_Helper : NSObject
+
+@end
---
+++ Pod/Classes/CodFis+Helper.m
@@ -0,0 +1,13 @@
+//
+//  CodFis+Helper.m
+//  Pods
+//
+//  Created by Giuseppe Nucifora on 08/07/15.
+//
+//
+
+#import "CodFis+Helper.h"
+
+@implementation CodFis_Helper
+
+@end
